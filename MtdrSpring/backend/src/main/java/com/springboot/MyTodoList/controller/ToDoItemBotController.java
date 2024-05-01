18444d0fca3f93bd78a234b7c6510d1e72fa5290
Ramón Gómez <a00832787@tec.mx>
2024-04-30 18:42:16 -0600

    cambio menor

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemBotController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemBotController.java
@@ -163,7 +163,7 @@ public class ToDoItemBotController extends TelegramLongPollingBot {
 				myTodoListTitleRow.add(BotLabels.MY_TODO_LIST.getLabel());
 				keyboard.add(myTodoListTitleRow);
 
-				List<ToDoItem> activeItems = allItems.stream().filter(item -> item.isDone() == false && item.getIdAssignee().equals(String.valueOf(user_id)))
+				List<ToDoItem> activeItems = allItems.stream().filter(item -> item.isDone() == false && (String.valueOf(user_id).equals(item.getIdAssignee())))
 						.collect(Collectors.toList());
 
 				for (ToDoItem item : activeItems) {
@@ -191,7 +191,7 @@ public class ToDoItemBotController extends TelegramLongPollingBot {
 					keyboard.add(currentRow);
 				}
 
-				List<ToDoItem> doneItems = allItems.stream().filter(item -> item.isDone() == true && item.getIdAssignee().equals(String.valueOf(user_id)))
+				List<ToDoItem> doneItems = allItems.stream().filter(item -> item.isDone() == true && (String.valueOf(user_id).equals(item.getIdAssignee())))
 						.collect(Collectors.toList());
 
 				for (ToDoItem item : doneItems) {
