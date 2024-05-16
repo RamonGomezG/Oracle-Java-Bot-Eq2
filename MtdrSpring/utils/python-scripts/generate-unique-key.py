6c06bc9b9c9769dec32c437a89fd98ed17caf95b
RaulKenyu <113393735+RaulKenyu@users.noreply.github.com>
2024-05-16 17:55:37 -0600

    Update generate-unique-key.py -line

--- MtdrSpring/utils/python-scripts/generate-unique-key.py
+++ MtdrSpring/utils/python-scripts/generate-unique-key.py
@@ -2,14 +2,12 @@ import random
 import string
 
 
-
 def get_random_key(length):
     letters = string.ascii_lowercase
     digits = string.digits
     length = length
     result_str = ''.join(random.choice(letters + digits) for i in range(length))
     print(result_str)
-
 
 
 get_random_key(5)
