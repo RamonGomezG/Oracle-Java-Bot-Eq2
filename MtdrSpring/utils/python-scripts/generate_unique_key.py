bcbb3242b449a7cafcb2968418e3e60cf734b8b9
RaulKenyu <113393735+RaulKenyu@users.noreply.github.com>
2024-05-16 18:22:00 -0600

    Update and rename generate-unique-key.py to generate_unique_key.py

--- MtdrSpring/utils/python-scripts/generate_unique_key.py
+++ MtdrSpring/utils/python-scripts/generate_unique_key.py
@@ -5,7 +5,6 @@ import string
 def get_random_key(length):
     letters = string.ascii_lowercase
     digits = string.digits
-    length = length
     result_str = ''.join(random.choice(letters + digits) for i in range(length))
     print(result_str)
 
