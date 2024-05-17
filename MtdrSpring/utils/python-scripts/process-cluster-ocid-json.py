43b9738a5ebac04b8eb3fc1f112ad63a43e7d569
RaulKenyu <113393735+RaulKenyu@users.noreply.github.com>
2024-05-16 18:06:22 -0600

    Update process-cluster-ocid-json.py remove e

--- MtdrSpring/utils/python-scripts/process-cluster-ocid-json.py
+++ MtdrSpring/utils/python-scripts/process-cluster-ocid-json.py
@@ -10,9 +10,9 @@ system_input = json.load(sys.stdin)
 try:
     OCID = system_input["lab_oke_cluster_id"]["value"]
     print(OCID)
-except KeyError as e:
+except KeyError:
     print("Error: Failed to access expected JSON key lab_oke_cluster_id or value")
-except json.decoder.JSONDecodeError as e:
+except json.decoder.JSONDecodeError:
     print("Error: Failed to decode JSON")
-except Exception as e:
+except Exception:
     print("Error: Encountered unexpected error")
