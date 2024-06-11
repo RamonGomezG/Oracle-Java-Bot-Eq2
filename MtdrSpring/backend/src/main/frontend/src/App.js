3a848a9fe794b78d9c09fbd98b0c4727297961e8
Alexander Alexeev <alexbogaychuk@gmail.com>
2024-06-11 17:53:34 -0600

    Cambio de filtrado por usuario

--- MtdrSpring/backend/src/main/frontend/src/App.js
+++ MtdrSpring/backend/src/main/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import NewItem from './NewItem';
+import UserSelector from './UserSelector'; // Asegúrate de tener este componente
 import API_LIST from './API';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Button, TableBody, TableCell, TableRow, CircularProgress, Table } from '@mui/material';
@@ -9,10 +10,46 @@ function App() {
   const [isLoading, setLoading] = useState(false);
   const [isInserting, setInserting] = useState(false);
   const [items, setItems] = useState([]);
+  const [currentUser, setCurrentUser] = useState('');
+  const [userIds, setUserIds] = useState([]);
   const [error, setError] = useState();
 
+  useEffect(() => {
+    reloadItems();
+  }, []);
+
+  useEffect(() => {
+    const ids = items.map(item => item.idAssignee).filter((value, index, self) => value && self.indexOf(value) === index);
+    setUserIds(ids);
+    // Opcional: Establecer un usuario por defecto
+    if (ids.length > 0 && !currentUser) {
+      setCurrentUser(ids[0]);
+    }
+  }, [items]);
+
+  function reloadItems() {
+    setLoading(true);
+    fetch(API_LIST)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Algo pasó mal ...');
+      }
+    })
+    .then(data => {
+      setLoading(false);
+      setItems(data);
+    })
+    .catch(error => {
+      console.error("Error al hacer el fetch:", error);
+      setLoading(false);
+      setError("Error al cargar elementos.");
+    });
+  }
+
   function deleteItem(deleteId) {
-    fetch(API_LIST + "/" + deleteId, {
+    fetch(`${API_LIST}/${deleteId}`, {
       method: 'DELETE',
       headers: {
         'X-Frame-Options': 'DENY',
@@ -32,43 +69,6 @@ function App() {
     });
   }
 
-  function toggleDone(event, id, description, done, details, priority, complexity) {
-    event.preventDefault();
-    modifyItem(id, description, done, details, priority, complexity).then(
-      updatedItem => {
-        const updatedItems = items.map(item => item.id === id ? updatedItem : item);
-        setItems(updatedItems);
-      },
-      error => {
-        console.error("Error actualizando elemento:", error);
-        setError("Fallo al actualizar el elemento.");
-      }
-    );
-  }
-
-  function modifyItem(id, description, done, details, priority, complexity) {
-    var data = { description, done, details, priority, complexity };
-    return fetch(API_LIST + "/" + id, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Frame-Options': 'DENY',
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Algo pasó mal ...');
-      }
-    });
-  }
-
-  useEffect(() => {
-    reloadItems();
-  }, []);
-
   function addItem(newItem){
     setInserting(true);
     fetch(API_LIST, {
@@ -80,52 +80,33 @@ function App() {
       body: JSON.stringify(newItem),
     }).then(response => {
       if (response.ok) {
-        reloadItems();  // Reload all items to see the new addition
+        reloadItems(); // Reload all items to see the new addition
         setInserting(false);
       } else {
         throw new Error('Algo pasó mal ...');
       }
     }).catch(error => {
-      console.error("Error actualizando elemento:", error);
+      console.error("Error añadiendo elemento:", error);
       setInserting(false);
-      setError("Error al actualizar elemento.");
-    });
-  }
-
-  function reloadItems() {
-    setLoading(true);
-    fetch(API_LIST)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Algo pasó mal ...');
-      }
-    })
-    .then(items => {
-      setLoading(false);
-      setItems(items);
-    })
-    .catch(error => {
-      console.error("Error al hacer el fetch:", error);
-      setLoading(false);
-      setError("Error al cargar elementos.");
+      setError("Error al añadir elemento.");
     });
   }
 
   return (
     <div className="App">
       <img src="/logo-transparente.png" alt="Logo" style={{ maxWidth: '200px', paddingBottom: '20px' }} />
+      {userIds.length > 0 && (
+        <UserSelector users={userIds} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      )}
       <NewItem addItem={addItem} isInserting={isInserting}/>
       {error && <p>Error: {error.message}</p>}
       {isLoading && <CircularProgress />}
       {!isLoading && (
         <>
-          <h2 className="section-header">🧑‍💻MIS DEVOPS TASKS🧑‍💻</h2>
           <Table>
             <TableBody>
-              {items.filter(item => !item.done).map(item => (
-                <TableRow key={item.id} className="not-done">
+              {items.filter(item => !item.done && item.idAssignee === currentUser).map(item => (
+                <TableRow key={item.id}>
                   <TableCell>{item.description}</TableCell>
                   <TableCell>{item.details}</TableCell>
                   <TableCell>{item.priority}</TableCell>
@@ -134,39 +115,13 @@ function App() {
                     <Moment format="MMM Do YY">{item.creation_ts}</Moment>
                   </TableCell>
                   <TableCell>
-                    <Button variant="contained" onClick={(event) => toggleDone(event, item.id, item.description, !item.done, item.details, item.priority, item.complexity)} size="small">
-                      Terminada ✅
+                    <Button variant="contained" onClick={() => toggleDone(item.id, item.description, !item.done, item.details, item.priority, item.complexity)} size="small">
+                      Done
                     </Button>
                   </TableCell>
                   <TableCell>
                     <Button startIcon={<DeleteIcon />} variant="contained" onClick={() => deleteItem(item.id)} size="small">
-                      Eliminar
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <h2 className="section-header">🧑‍💻TASKS TERMINADAS🧑‍💻</h2>
-          <Table>
-            <TableBody>
-              {items.filter(item => item.done).map(item => (
-                <TableRow key={item.id} className="done">
-                  <TableCell>{item.description}</TableCell>
-                  <TableCell>{item.details}</TableCell>
-                  <TableCell>{item.priority}</TableCell>
-                  <TableCell>{item.complexity}</TableCell>
-                  <TableCell>
-                    <Moment format="MMM Do YY">{item.creation_ts}</Moment>
-                  </TableCell>
-                  <TableCell>
-                    <Button variant="contained" onClick={(event) => toggleDone(event, item.id, item.description, !item.done, item.details, item.priority, item.complexity)} size="small">
-                      Deshacer
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button startIcon={<DeleteIcon />} variant="contained" onClick={() => deleteItem(item.id)} size="small">
-                      Eliminar
+                      Delete
                     </Button>
                   </TableCell>
                 </TableRow>
